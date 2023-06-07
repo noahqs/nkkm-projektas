0a2d9fa058987f50b834230f181ad4f06c63072b
nojusbr <nojus.braziulis@gmail.com>
2023-06-07 10:37:28 +0300

    Bug fixes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
